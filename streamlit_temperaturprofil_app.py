6af4981b7a59c7b9465052e380f10f6921f91dc2
dubbehendrik <145358460+dubbehendrik@users.noreply.github.com>
2025-05-16 23:09:44 +0200

    Update streamlit_temperaturprofil_app.py

--- streamlit_temperaturprofil_app.py
+++ streamlit_temperaturprofil_app.py
@@ -11,12 +11,11 @@ st.set_page_config(layout="wide")
 
 col_title, col_logo = st.columns([6, 1])
 
-with col_title:
-    st.title("Bestimmung des Wärmeübergangskoeffizienten")
-
 with col_logo:
     st.image("HSE-Logo.jpg", width=1000)
 
+with col_title:
+    st.title("Bestimmung des Wärmeübergangskoeffizienten")
 
 # Hinweistext
 with st.expander("ℹ️ Hinweise zur Verwendung"):
